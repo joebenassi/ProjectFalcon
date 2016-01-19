6ff45daf56b6f98fd6a0bf8d671bbaa549ca4c8e
JonathanLipsky <lokidog2006@yahoo.com>
2016-01-19 15:48:26 -0600

    Title Change
    Pig

--- appClientModule/GUI.java
+++ appClientModule/GUI.java
@@ -6,7 +6,7 @@ public class GUI extends JFrame {
     }
 
     private void initUI() {
-        setTitle("Project Flacon");
+        setTitle("Project Flacon!");
         setSize(1000, 1000);
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
