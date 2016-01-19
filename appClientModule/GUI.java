9c259fb6ca2a6fbc18f337b0c767a1e15a421f18
Joe Benassi <joebenassi@gmail.com>
2016-01-19 15:38:51 -0600

    Add GUI

---
+++ appClientModule/GUI.java
@@ -0,0 +1,14 @@
+import javax.swing.JFrame;
+
+public class GUI extends JFrame {
+	public GUI() {
+        initUI();
+    }
+
+    private void initUI() {
+        setTitle("Project Flacon");
+        setSize(1000, 1000);
+        setLocationRelativeTo(null);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+    }
+}
--- appClientModule/Main.java
+++ appClientModule/Main.java
@@ -1,14 +1,10 @@
-
 public class Main {
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		GUI gui = new GUI();
+		gui.setVisible(true);
 	}
 
-	/* (non-Java-doc)
-	 * @see java.lang.Object#Object()
-	 */
 	public Main() {
 		super();
 	}
-
 }
