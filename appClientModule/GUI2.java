c3774a3e9c7b3df9a9728b69ce6deec14c35a373
Joe Benassi <joebenassi@gmail.com>
2016-01-19 16:06:16 -0600

    Add true/false gen

--- appClientModule/GUI2.java
+++ appClientModule/GUI2.java
@@ -1,4 +1,13 @@
+import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.beans.PropertyChangeListener;
+
+import javax.swing.Action;
+import javax.swing.JButton;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
+import java.util.Random;
 
 public class GUI2 extends JFrame {
 	public GUI2() {
@@ -7,7 +16,21 @@ public class GUI2 extends JFrame {
 
     private void initUI() {
         setTitle("Project Flacon! (Joe version)");
-        setSize(1000, 1000);
+        setSize(250, 250);
+        setLayout(new BorderLayout());
+        add(new JLabel("PROJECT FALCON"), BorderLayout.NORTH); //title
+        JLabel printed = new JLabel(); //output
+        
+        JButton btn = new JButton("Print Num"); //button, middle
+        btn.addActionListener(new ActionListener() {
+        	public void actionPerformed(ActionEvent e) {
+        		printed.setText(new Random().nextInt(Integer.MAX_VALUE) % 2 == 0 ? "TRUE" : "FALSE");
+        	}
+        });
+        
+        add(btn, BorderLayout.CENTER);
+        add(printed, BorderLayout.SOUTH);
+        
         setLocationRelativeTo(null);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
     }
