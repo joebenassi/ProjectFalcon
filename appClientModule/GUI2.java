24bd2cc2ebb18ae304fe7d4e87b8a854b3a9c620
Joe Benassi <joebenassi@gmail.com>
2016-01-19 16:24:58 -0600

    Add random number gen

--- appClientModule/GUI2.java
+++ appClientModule/GUI2.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.Dimension;
+import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.beans.PropertyChangeListener;
@@ -7,6 +9,9 @@ import javax.swing.Action;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JTextField;
+
 import java.util.Random;
 
 public class GUI2 extends JFrame {
@@ -16,19 +21,38 @@ public class GUI2 extends JFrame {
 
     private void initUI() {
         setTitle("Project Flacon! (Joe version)");
-        setSize(250, 250);
+        setSize(250, 150);
         setLayout(new BorderLayout());
         add(new JLabel("PROJECT FALCON"), BorderLayout.NORTH); //title
         JLabel printed = new JLabel(); //output
+        JTextField minTextBox = new JTextField();
+        minTextBox.setMinimumSize(new Dimension(10, 25));
+        JTextField maxTextBox = new JTextField();
+        maxTextBox.setMinimumSize(new Dimension(10, 25));
         
         JButton btn = new JButton("Print Num"); //button, middle
         btn.addActionListener(new ActionListener() {
         	public void actionPerformed(ActionEvent e) {
-        		printed.setText(new Random().nextInt(Integer.MAX_VALUE) % 2 == 0 ? "TRUE" : "FALSE");
+        		String cell1text = minTextBox.getText();
+        		String cell2text = maxTextBox.getText();
+        		
+        		try {
+        			int min = Integer.parseInt(cell1text);
+        			int max = Integer.parseInt(cell2text);
+        			printed.setText((new Random().nextInt(max-min+1) + min) + "");
+        		} catch (Exception ex) {
+        			// do nothing
+        		}
         	}
         });
         
-        add(btn, BorderLayout.CENTER);
+        JPanel textPanel = new JPanel();
+        textPanel.setLayout(new GridLayout(1, 2));
+        textPanel.add(minTextBox);
+        textPanel.add(maxTextBox);
+
+        add(textPanel, BorderLayout.CENTER);
+        add(btn, BorderLayout.EAST);
         add(printed, BorderLayout.SOUTH);
         
         setLocationRelativeTo(null);
